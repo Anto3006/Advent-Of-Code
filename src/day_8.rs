f458e688026dae25b84b7663a9f1a5962286dbfa
Antonio Piedra <anto.piedrapach@hotmail.com>
2024-09-07 18:56:40 -0600

    Day 8: part 2

--- src/day_8.rs
+++ src/day_8.rs
@@ -2,10 +2,10 @@ use std::fs;
 
 fn main() {
     println!("This is day 8 of advent of code 2015");
-    let input = fs::read_to_string("inputs/day_8/part_1.txt").unwrap();
+    let input = fs::read_to_string("inputs/day_8/input.txt").unwrap();
     let mut diff = 0;
     for line in input.lines() {
-        diff += line.chars().count() - count_chars(line);
+        diff += calculate_encoded_string_size(line) - line.chars().count();
     }
     println!("Result: {diff}");
 }
@@ -38,6 +38,19 @@ fn count_chars(str: &str) -> usize {
     count
 }
 
+fn calculate_encoded_string_size(string: &str) -> usize {
+    let mut size = 0;
+    for c in string.chars() {
+        if c == '\"' || c == '\\' {
+            size += 2;
+        } else {
+            size += 1;
+        }
+    }
+    size += 2;
+    size
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -56,5 +69,20 @@ mod tests {
         assert_eq!(count_chars(third_test), 7);
         assert_eq!(count_chars(fourth_test), 3);
         assert_eq!(count_chars(fifth_test), 2);
+    }
+
+    #[test]
+    fn test_calculate_encoded_size() {
+        let first_test = "\"\"";
+        let second_test = "\"aaa\"";
+        let third_test = "\"aaa\\\"aaa\"";
+        let fourth_test = "\"a\\a\"";
+        let fifth_test = "\"\\x23\"";
+
+        assert_eq!(calculate_encoded_string_size(first_test), 6);
+        assert_eq!(calculate_encoded_string_size(second_test), 9);
+        assert_eq!(calculate_encoded_string_size(third_test), 16);
+        assert_eq!(calculate_encoded_string_size(fourth_test), 10);
+        assert_eq!(calculate_encoded_string_size(fifth_test), 11);
     }
 }
